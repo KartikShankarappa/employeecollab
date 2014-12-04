1258e0c5d9ada962192105583fb976a57977ad3f
Afroze Khan <afroze.vy349@gmail.com>
2014-12-03 21:41:49 -0800

    User registration and login complete

--- views/browser.py
+++ views/browser.py
@@ -4,7 +4,8 @@ from functools import wraps
 import os,md5
 from werkzeug import secure_filename
 from datetime import timedelta, datetime
-
+from models.usermodel import UserModel
+from models.user_objects import *
 
 def login_required(func):
     """
@@ -23,5 +24,34 @@ def home():
     return render_template('master.html')
 
 @app.route('/wall')
+@login_required
 def wall():
-    return render_template('index.html')
+    return render_template('index.html', fname = session['user']['first_name'])
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    fields = users()
+    model_object = UserModel()
+    fields.email_id = request.form["txtusername"]
+    fields.password = request.form["txtuserpassword"]
+    if model_object.login(fields):
+        return "1"
+    return "0"
+
+@app.route('/logout')
+def logout():
+    if session.has_key('user'):
+        session.pop("user")
+    return redirect("/")
+
+@app.route('/register', methods=["POST"])
+def register():
+    fields = users()
+    model_object = UserModel()
+    fields.first_name = request.form["txtfname"]
+    fields.last_name = request.form["txtlname"]
+    fields.email_id = request.form["txtemail"]
+    fields.password = request.form["txtpassword"]
+    model_object.register_user(fields)
+    return "text"
--- views/static/js/footer.js
+++ views/static/js/footer.js
@@ -5,4 +5,27 @@ $(document).ready(function(){
 		$('#fileupload').toggle();
 	})
 
+	$('#formregister').submit(function(event){
+		event.preventDefault();
+		form_data = $(this).serialize();
+
+		$.post('/register', form_data, function(data,status){
+            alert(data);
+        });
+	});
+
+	$('#formlogin').submit(function(event){
+		event.preventDefault();
+		form_data = $(this).serialize();
+
+		$.post('/login', form_data, function(data,status){
+            if(data == "1"){
+            	window.location.replace("/wall");
+            }
+            else{
+            	$('#loginwarn').text("Invalid login");
+            }
+        });
+	});
 });
+
